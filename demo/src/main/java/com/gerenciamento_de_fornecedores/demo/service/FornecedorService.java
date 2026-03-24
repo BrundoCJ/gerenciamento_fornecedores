11f178746d369b32b50667301195a7a65852f358
BrundoCJ <bconrado04@gmail.com>
2026-03-24 17:08:10 -0300

    adicionando service

--- demo/src/main/java/com/gerenciamento_de_fornecedores/demo/service/FornecedorService.java
+++ demo/src/main/java/com/gerenciamento_de_fornecedores/demo/service/FornecedorService.java
@@ -1,4 +1,38 @@
 package com.gerenciamento_de_fornecedores.demo.service;
 
+import com.gerenciamento_de_fornecedores.demo.entity.Fornecedor;
+import com.gerenciamento_de_fornecedores.demo.repository.FornecedorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FornecedorService {
+
+    private final FornecedorRepository fornecedorRepository;
+
+    public FornecedorService(FornecedorRepository fornecedorRepository) {
+        this.fornecedorRepository = fornecedorRepository;
+    }
+
+    public Fornecedor salvar(Fornecedor fornecedor) {
+        return fornecedorRepository.save(fornecedor);
+    }
+
+    public List<Fornecedor> listarTodos() {
+        return fornecedorRepository.findAll();
+    }
+
+    public Optional<Fornecedor> buscarPorId(Long id) {
+        return fornecedorRepository.findById(id);
+    }
+
+    public boolean deletar(Long id) {
+        if (fornecedorRepository.existsById(id)) {
+            fornecedorRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
