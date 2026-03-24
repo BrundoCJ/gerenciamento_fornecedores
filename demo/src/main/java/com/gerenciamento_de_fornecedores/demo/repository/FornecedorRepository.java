2c648b0bda751bd3a153f9e128759488ee30574b
BrundoCJ <bconrado04@gmail.com>
2026-03-24 17:07:44 -0300

    adicionando repository

--- demo/src/main/java/com/gerenciamento_de_fornecedores/demo/repository/FornecedorRepository.java
+++ demo/src/main/java/com/gerenciamento_de_fornecedores/demo/repository/FornecedorRepository.java
@@ -1,4 +1,9 @@
 package com.gerenciamento_de_fornecedores.demo.repository;
 
-public class FornecedorRepository {
+import com.gerenciamento_de_fornecedores.demo.entity.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 }
